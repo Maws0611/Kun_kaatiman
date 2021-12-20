b87ffeb0e92fc1051e7a18aa58dcddf92709d701
Asta Séne <astasene81@gmail.com>
2021-12-20 09:14:48 +0000

    modifaication

--- kunkaatiman.js
+++ kunkaatiman.js
@@ -1,12 +1,12 @@
 
-let togg1 = document.getElementById("togg1");
-togg1.addEventListener("click", () => {
-  let d1 = document.getElementById("d1");
-  d1.style.visibility = 'visible'
-  togg1.style.visibility = 'hidden'
+let accueil = document.getElementById("accueil");
+accueil.addEventListener("click", () => {
+  let contenu_qassida = document.getElementById("contenu_qassida");
+  contenu_qassida.style.visibility = 'visible'
+  accueil.style.visibility = 'hidden'
  
 })
 
 window.onload = function(){
-  document.getElementById("son1").play()
+  document.getElementById("son").play()
 }
